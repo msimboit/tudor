0e61ca8b3576c1ba0e4ce535fc4c0a8978218a5a
Msimboit <msimboit@gmail.com>
2021-08-09 13:31:50 +0300

    a

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
@@ -81,7 +82,7 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header d-flex justify-content-between"><span>{{ __('Scanner') }}</span>  <span>{{ \Carbon\Carbon::now()->toDateString() }}</span></div>
-                    <video id="preview"></video>
+                    <video id="preview" playsinline></video>
                     <div class="card-body d-flex flex-column justify-content-center align-items-center">
                         <!-- <div id="mapholder"></div> -->
 
