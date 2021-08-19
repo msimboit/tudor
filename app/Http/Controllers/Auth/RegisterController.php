e95ae87ab9321f9987c4a5f46222ac0ded92a774
Msimboit <msimboit@gmail.com>
2021-08-19 14:56:15 +0300

    User Seeder updated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,4 +74,9 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    public function changePassword()
+    {
+        return view ('changePassword');
+    }
 }
