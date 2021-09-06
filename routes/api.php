ba0ef64507b0d50ded53ddc5bc652bf8adcbaf64
Msimboit <msimboit@gmail.com>
2021-09-06 10:09:33 +0300

    Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UsersController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ScannersController;
 use App\Http\Controllers\Api\ShiftsController;
 use App\Http\Controllers\Api\IssuesController;
@@ -21,6 +22,8 @@ use App\Http\Controllers\ChatController;
 */
 
 Route::prefix('v1')->group(function() {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
     Route::apiResource('/users', UsersController::class);
     Route::apiResource('/shifts', ShiftsController::class);
     Route::apiResource('/scans', ScannersController::class);
@@ -29,8 +32,6 @@ Route::prefix('v1')->group(function() {
 
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
 // Route::prefix('v1')->group(['middleware' => ['auth:sanctum']], function() {
 //     Route::apiResource('/users', UsersController::class);
@@ -38,5 +39,5 @@ Route::post('/login', [AuthController::class, 'login']);
 //     Route::apiResource('/scans', ScannersController::class);
 //     Route::apiResource('/issues', IssuesController::class);
 //     Route::apiResource('/chats', ChatController::class);
-
+//     Route::post('/logout', [AuthController::class, 'logout']);
 // });
