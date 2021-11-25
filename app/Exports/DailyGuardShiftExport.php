df7350783e24c0871694f1c56b72776b271392fc
Msimboit <msimboit@gmail.com>
2021-11-25 10:28:38 +0300

    Time

--- app/Exports/DailyGuardShiftExport.php
+++ app/Exports/DailyGuardShiftExport.php
@@ -28,25 +28,37 @@ class DailyGuardShiftExport implements FromQuery
         if($location == "Lang'ata")
         {
             $sectors = (QrCode::where('location', 'langata')->select('code')->get())->toArray();
-            return Scan::query()->whereIn('sector', $sectors)
+            return Scan::select(['id', 'first_name', 'sector_name', 'time', 'created_at'])
+                            ->whereIn('sector', $sectors)
                             ->where('role', 'guard')
-                            ->whereDate('created_at', '>', Carbon::now()->subDays(1));
+                            ->whereDate('created_at', '>', Carbon::now()->subDays(1))
+                            ->withCasts([
+                                'created_at' => 'datetime:Y-m-d'
+                            ]);
         }
 
         if($location == 'Baraka')
         {
             $sectors = (QrCode::where('location', 'baraka')->select('code')->get())->toArray();
-            return Scan::query()->where('sector', $sectors)
+            return Scan::select(['id', 'first_name', 'sector_name', 'time', 'created_at'])
+                            ->where('sector', $sectors)
                             ->where('role', 'guard')
-                            ->whereDate('created_at', '>', Carbon::now()->subDays(1));
+                            ->whereDate('created_at', '>', Carbon::now()->subDays(1))
+                            ->withCasts([
+                                'created_at' => 'datetime:Y-m-d'
+                            ]);
         }
 
         if($location == 'Allimex')
         {
             $sectors = (QrCode::where('location', 'allimex')->select('code')->get())->toArray();
-            return Scan::query()->where('sector', $sectors)
+            return Scan::select(['id', 'first_name', 'sector_name', 'time', 'created_at'])
+                            ->where('sector', $sectors)
                             ->where('role', 'guard')
-                            ->whereDate('created_at', '>', Carbon::now()->subDays(1));
+                            ->whereDate('created_at', '>', Carbon::now()->subDays(1))
+                            ->withCasts([
+                                'created_at' => 'datetime:Y-m-d'
+                            ]);
         }
     }
 }
