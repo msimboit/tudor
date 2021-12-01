3b89a08f5e4eabf56a770631816f3f04b528a156
Msimboit <msimboit@gmail.com>
2021-12-01 15:21:47 +0300

    Geolocation

--- resources/views/employees/map.blade.php
+++ resources/views/employees/map.blade.php
@@ -27,9 +27,10 @@
     var marker, i;
     for (i = 0; i < locations.length; i++) {  
         console.log(locations[i][1]);
-
         var indicator = Array.from(locations[i][0]);
         console.log(indicator[0]);
+        console.log(locations[i][2]);
+        console.log(typeof locations[i][0]);
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
             map: map,
