6b811f8bccb03abe954b6285ef1529b519e31679
Msimboit <msimboit@gmail.com>
2021-12-03 11:11:57 +0300

    Scan Correct Code

--- app/Http/Controllers/ScannerController.php
+++ app/Http/Controllers/ScannerController.php
@@ -77,10 +77,10 @@ class ScannerController extends Controller
             return redirect()->back()->with('alert', 'Please Enable Your Location!');
         }
 
-        if(($request->sector_name) == 'Clocking In')
-        {
-            return redirect()->back()->with('alert', 'Please scan the correct code!');
-        }
+        // if(($request->sector_name) == 'Clocking In')
+        // {
+        //     return redirect()->back()->with('alert', 'Please scan the correct code!');
+        // }
 
         $validated = $request->validate([
             'sector' => 'required',
