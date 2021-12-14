e0df9805b82d89a0307685c01aa3041d1ec6b8ef
Msimboit <msimboit@gmail.com>
2021-12-14 15:13:40 +0300

    Scan API

--- app/Http/Controllers/Api/ScannersController.php
+++ app/Http/Controllers/Api/ScannersController.php
@@ -65,7 +65,7 @@ class ScannersController extends Controller
 
         $actual_scan_time = Carbon::now();
         $diff = (Carbon::parse($last_clock_in->created_at))->diffInHours($actual_scan_time);
-        if($diff > 12 && ( ($request->sector !== 'TCS000201') || ($request->sector !== 'TCS00101') )){
+        if($diff > 12 && ( ($request->sector != 'TCS000201') || ($request->sector != 'TCS00101') )){
             $response = [
                 'message' => 'Clock In First',
             ];
